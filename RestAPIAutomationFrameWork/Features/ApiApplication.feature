8ac632a4526d08bfed330141cc6e24312bf232c1
nipuns11 <nipunsh19@gmail.com>
2019-02-13 21:24:10 -0500

    rest api fw
    rest api fw

---
+++ RestAPIAutomationFrameWork/Features/ApiApplication.feature
@@ -0,0 +1,11 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given I have a endpoint /endpoint/
+	And I have a base url http://mydomain.com/
+	When I call get method of the api
+	Then the I get the API response in json format
---
+++ RestAPIAutomationFrameWork/Steps/ApiApplication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace RestAPIAutomationFrameWork.Steps
+{
+    [Binding]
+    public sealed class ApiApplication
+    {
+        [Given(@"I have a endpoint (.*)")]
+        public void GivenIHaveAEndpointEndpoint(string endpoint)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have a base url (.*)")]
+        public void GivenIHaveABaseUrl(string baseURL)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I call get method of the api")]
+        public void WhenICallGetMethodOfTheApi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the I get the API response in json format")]
+        public void ThenTheIGetTheAPIResponseInJsonFormat()
+        {
+            ScenarioContext.Current.Pending();
+            // checkc 
+        }
+
+    }
+}
