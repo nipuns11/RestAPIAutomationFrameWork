122ecfb119b2d367c2748b094554c45f1c029e31
nipuns11 <nipunsh19@gmail.com>
2019-02-20 11:38:26 -0500

    updated api tests
    updated api tests

---
+++ RestAPIAutomationFrameWork/RestApiHelper.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPIAutomationFrameWork
+{
+    public static class RestApiHelper
+    {
+        public static RestClient client;
+        public static RestRequest restRequest;
+        public static string baseurl = "http://mydomain.com";
+
+        public static RestClient SetURL(string endpoint)
+        {
+            var url = Path.Combine(baseurl, endpoint);
+            return client =  new RestClient(url);
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Accept","application/json");
+            return restRequest;
+        }
+
+        public static IRestResponse GetResponse()
+        {
+            return client.Execute(restRequest);
+        }
+        
+    }
+}
